dd1d29f691fd9a5405c22d46a1921ef592bf50d9
nacho <i.castillejogomez@gmail.com>
2018-11-12 08:33:32 +0100

    Updated Readme.md

---
+++ src/github.com/icastillejogomez/CursoGo/HolaMundo/main.go
@@ -0,0 +1,9 @@
+package main
+
+// Importamos la libreria para poder imprimir mensajes por consola
+import "fmt"
+
+// Punto de entrada de nuestro programa
+func main() {
+	fmt.Print("Hola mundo\n")
+}
